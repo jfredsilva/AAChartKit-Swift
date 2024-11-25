2ca325682e7a3770eaa4adee32b58006ff03c8ae
Fred Silva <fred.silva@avanceesoftware.com>
2024-11-25 10:52:20 +0000

    Added methods to open/close tooltip

--- AAInfographics/AAChartCreator/AAChartUIView.swift
+++ AAInfographics/AAChartCreator/AAChartUIView.swift
@@ -11,15 +11,18 @@ import SwiftUI
 @available(iOS 13.0, macOS 10.15, *)
 public struct AAChartUIView : UIViewRepresentable {
     
+    @State private var isTooltipOpen: Bool = false
+    
     @Binding private var model: AAChartModel
     @Binding private var series: [AASeriesElement]
     @Binding private var options: AAOptions?
+    @Binding private var selectedPoint: Int?
     
-    public init(model: Binding<AAChartModel>, series: Binding<[AASeriesElement]>, options: Binding<AAOptions?> = .constant(nil)) {
+    public init(model: Binding<AAChartModel>, series: Binding<[AASeriesElement]>, options: Binding<AAOptions?> = .constant(nil), selectedPoint: Binding<Int?> = .constant(nil)) {
         _options = options
-
         _series = series
         _model = model
+        _selectedPoint = selectedPoint
     }
     
     public func makeUIView(context: Context) -> UIView {
@@ -35,9 +38,6 @@ public struct AAChartUIView : UIViewRepresentable {
     }
     
     public func updateUIView(_ uiView: UIView, context: Context) {
-//        guard let series = self.model.series as? [AASeriesElement] else {
-//            return
-//        }
         
         let coordinator = context.coordinator
 
@@ -46,17 +46,45 @@ public struct AAChartUIView : UIViewRepresentable {
         } else {
             coordinator.chartView.aa_refreshChartWholeContentWithChartModel(self.model)
         }
-        //if self.model.xAxisLabelsEnabled ?? true {
-//            coordinator.chartView.aa_refreshChartWholeContentWithChartModel(self.model)
-        //} else {
-//            coordinator.chartView.aa_onlyRefreshTheChartDataWithChartModelSeries(series, animation: false)
-        //}
+        
+        if let selectedPoint = self.selectedPoint {
+            self.openTooltip(position: selectedPoint, coordinator: coordinator)
+        } else if self.isTooltipOpen {
+            self.closeTooltip(coordinator: coordinator)
+        }
     }
     
     //-- MARK: coordinator
     
     public func makeCoordinator() -> AAChartCoordinator {
         AAChartCoordinator(self)
+    }
+    
+    private func openTooltip(position: Int, coordinator: AAChartCoordinator) {
+        self.safeEvaluate(javascript: "aaGlobalChart.series[0].points[\(position)].onMouseOver();", coordinator: coordinator)
+        
+        
+        if self.isTooltipOpen == false {
+            DispatchQueue.main.async {
+                self.isTooltipOpen = true
+            }
+        }
+    }
+    
+    private func closeTooltip(coordinator: AAChartCoordinator) {
+        self.safeEvaluate(javascript: "aaGlobalChart.onMouseOut();", coordinator: coordinator)
+            
+        DispatchQueue.main.async {
+            self.isTooltipOpen = false
+        }
+    }
+    
+    public func safeEvaluate(javascript: String, coordinator: AAChartCoordinator) {
+        coordinator.chartView.evaluateJavaScript(javascript) { _, error in
+            if let error = error {
+                print("Error evaluating JavaScript: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
